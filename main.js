e1190ee9e77bd43a0531f26e1ffca1cc54e4166d
EvelynJames9 <98927398+EvelynJames9@users.noreply.github.com>
2022-09-29 19:48:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
@@ -9,48 +14,33 @@ function setup() {
 	canvas.parent('canvas');
 
 	instializeInSetup(mario);
-
+	
 	video = createCapture(VIDEO);
 	video.size(800,400);
 	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
+
 }
 
-function modelLoaded(){
-	console.log("PoseNet Model Loaded");
-}
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
 
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+   
 function draw() {
 	game();
 }
-
-function gotPoses(results){
-	if(results.length > 0){
-		console.log(results);
-		noseX = results[0].pose.nose.x;
-		noseY = results[0].pose.nose.y;
-	}
-}
-
-noseX = "";
-noseY = "";
-GameStatus = "";
-
-function startGame(){
-	GameStatus = "start";
-	document.getElementById("status").innerHTML = "Game Is Loading";
-}
-
-function game(){
-	console.log("noseX = " + noseX + " ,noseY = "+noseY);
-
-	instializeInDraw();
-	moveEnviroment(mario);
-	drawSprites();
-}
-
 
 
 
